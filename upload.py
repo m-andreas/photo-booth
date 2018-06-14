9c371b23f1bcb9ca7747ae96180f6e3c8d6b5f70
m-andreas <mar.andreas@yahoo.de>
2018-06-14 23:35:48 +0200

    Update upload.py

--- upload.py
+++ upload.py
@@ -1,1 +1,6 @@
-
+import flickrapi
+FLICKR_PUBLIC = '16b19f04436708b6b4b8465b9e12b5f0'
+FLICKR_SECRET = '0a67d76bc0a6bf8a'
+flickr = flickrapi.FlickrAPI(api_key, api_secret)
+flickr.authenticate_via_browser(perms='write')
+flickr.upload(filename="/tmp/c.jpg", title="test", is_public=1, format="rest")
